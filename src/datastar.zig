622f7b63f4df29532d882e787bed20e32c0e7273
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2025-08-12 10:43:27 +1000

    silly bug - you can now call msg.swapTo(Command) to change the type of msg, eg - start with patch elements, then swap to patch signals in the same stream

--- src/datastar.zig
+++ src/datastar.zig
@@ -58,7 +58,6 @@ pub const Message = struct {
     only_if_missing: bool = false,
     line_in_progress: bool = false,
     keep_script: bool = false,
-    protocol: Command = .patchElements,
 
     const Writer = std.io.Writer(
         *Message,
@@ -82,13 +81,14 @@ pub const Message = struct {
         return m;
     }
 
-    pub fn messageType(self: *Message, protocol: Command) void {
-        if (self.protocol == protocol) {
+    pub fn swapTo(self: *Message, command: Command) void {
+        if (self.command == command) {
             return;
         }
-        // swap to new protocol
+        // swap to new command
         self.end();
-        self.protocol = protocol;
+        self.command = command;
+        std.debug.print("swapped to command {}\n", .{self.command});
     }
 
     pub fn end(self: *Message) void {
@@ -99,6 +99,7 @@ pub const Message = struct {
     }
 
     pub fn header(self: *Message) !void {
+        std.debug.print("D* header with command protocol {}\n", .{self.command});
         var w = self.stream.writer();
         switch (self.command) {
             .patchElements => {
@@ -132,6 +133,7 @@ pub const Message = struct {
 
     pub fn write(self: *Message, bytes: []const u8) !usize {
         if (!self.started) {
+            std.debug.print("write and not started - header {}\n", .{self.command});
             try self.header();
         }
 
